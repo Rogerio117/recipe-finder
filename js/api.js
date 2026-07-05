e9ecb15b096b48c0494c9f6b5e82380c75f21e22
Rogerio Emmanuel Canto Romero <rogerio_canto@MacBook-Air-de-Rogerio.local>
2026-07-05 08:02:05 -0600

    Add Spoonacular API functions

---
+++ js/api.js
@@ -0,0 +1,43 @@
+async function searchRecipes(query, diet = '') {
+
+    try {
+
+        const dietParam = diet ? `&diet=${diet}` : ''
+        const url = `${CONFIG.BASE_URL}/recipes/complexSearch?query=${query}${dietParam}&number=12&addRecipeInformation=true&apiKey=${CONFIG.API_KEY}`
+
+        const response = await fetch(url)
+
+        const data = await response.json()
+
+        console.log(data.results)
+
+        return data.results
+    } catch (error) {
+        console.error('Error fetching recipes:', error)
+        throw error
+    }
+
+}
+
+async function getRecipeById(id) {
+
+    try {
+
+        const url = `${CONFIG.BASE_URL}/recipes/${id}/information?&apiKey=${CONFIG.API_KEY}`
+        
+        const response = await fetch(url)
+
+        const data = await response.json()
+
+        console.log(data)
+
+        return data
+
+    } catch (error){
+
+        console.error('Error fetching recipes detail:', error)
+        throw error
+
+    }
+    
+}
